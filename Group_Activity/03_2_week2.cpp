15fac8998277fd7cac50f78467416c98518cb7ff
ysy <yskite09@gmail.com>
2024-09-12 11:12:12 +0900

    Save Group_Activity_week2

--- Group_Activity/03_2_week2.cpp
+++ Group_Activity/03_2_week2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+typedef int (*myfunc)(int, int);        // function pointer 정의 가능
+
 int addition(int a, int b)
 {
     return a + b;
@@ -11,21 +13,23 @@ int subtraction(int a, int b)
     return a - b;
 }
 
-int operation(int x, int y, int (*functocall)(int, int))
+int operation(int x, int y, myfunc functocall)
 {
     int g;
-    g = (*functocall)(x, y);
+    g = functocall(x, y);
     return (g);
 }
 
 int main()
 {
     int m, n;
-    int (*minus)(int, int) = subtraction;   // 함수 포인터
+    //int (*minus)(int, int) = subtraction;   // 함수 포인터
 
+    myfunc minus = subtraction;
     m = operation(7, 5, addition);      // 12
     n = operation(20, m, minus);        // 8
 
-    cout << n;                          // 8
+    cout << m << endl;
+    cout << n << endl;                  // 8
     return 0;
 }
