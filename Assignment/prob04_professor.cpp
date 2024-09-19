b44ed73741fd169131e88478f6b548c157bd8ea4
Glesep <yskite09@gmail.com>
2024-09-19 11:18:48 +0900

    Save prob04_professor.cpp

---
+++ Assignment/prob04_professor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream infile("input4.txt");
+
+    string word;            // 한 단어를 받는 변수 정의
+    string words[1000];     // 단어들을 저장하는 변수 정의
+    int words_count = 0;    // words 배열 내의 단어 개수
+    //bool duplicated;
+
+    while(infile>>word) {
+        bool duplicated = false; // duplicated false, 변수는 사용할 때 선언, 스코프를 최대한 좁게 만들자
+        // infile >> word;
+        
+        if (word.length() < 2)                       // 길이가 2 이상이 아닐 떄 다음 반복으로 넘어감
+            continue;
+        
+        for (int i = 0; i < words_count; i++) {
+            if (words[i] == word) {                  // 단어가 같을 시, duplicated = true     
+                duplicated = true;
+                break;
+            }
+        }
+
+        if (duplicated == true)                     // 단어가 같을 시(duplicated = true), 다음 반복으로 넘어감
+            continue;
+
+        words[words_count++] = word;                  // 중복되지 않은 word를 words 배열에 추가
+
+
+        // if (infile.eof() == false)                  // 파일의 끝이 아닐 때에만 words_count를 추가, 너무 인덱스에 매몰되지 말자
+        //     words_count++;                             
+    }
+
+    infile.close();
+    
+    for (int i = 0; i < words_count; i++) {
+        cout << words[i];
+        if (i != words_count)
+            cout << endl;
+    }
+       
+        
+    return 0;
+}
