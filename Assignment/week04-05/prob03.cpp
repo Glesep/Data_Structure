a7199822f1cabeae549f9d0db095cd95d1a856c6
SangYeon Yoon <yskite09@gmail.com>
2024-09-25 22:34:11 +0900

    Edit prob_03.cpp

--- Assignment/week04-05/prob03.cpp
+++ Assignment/week04-05/prob03.cpp
@@ -20,83 +20,66 @@ using namespace std;
 find_if() 함수
 https://modoocode.com/263
 */
-pair<string, string> split_line(string &line, char delimiter);
+// pair<string, string> split_line(string &line, char delimiter);
+void store_wordsAndDiscriptions(string &line, vector<string> &words, vector<string> &discriptions);
 
 int main()
 {
-    vector<string> wordsAndDiscriptions;
-    // vector<string> descriptions;
+    vector<string> words;
+    vector<string> discriptions;
     string str;
 
     ifstream infile("shuffled_dict.txt");
 
     while (getline(infile, str))
     { // 1. 단어와 설명을 벡터에 저장
-        wordsAndDiscriptions.push_back(str);
+        store_wordsAndDiscriptions(str, words, discriptions);
     }
 
     infile.close();
 
     cin >> str; // 2. 한 단어를 키보드로부터 입력받기
 
+    for (int i = 0; i < (int)words.size() - 1; i++) {
+        if (str == words[i]) {
+            cout << words[i] << ": " << discriptions[i] << endl;    // 3. 사전에 있는 단어면 그 단어와 설명 출력
 
-
-
-    // 더욱 간단히 하는 법 생각해보기
-    for (auto it = wordsAndDiscriptions.begin(); it < wordsAndDiscriptions.end(); it++) {
-        pair<string, string> p = split_line(*it, '\t');
-        if (str == p.first) {                   // 3. 사전에 있는 단어면 그 단어와 설명 출력
-            cout << p.first << ": " << p.second << endl;
-
-            stringstream sstream(p.second);     // 4. 단어의 설명에 등장하는 길이가 3이상인 모든 단어들을 다시 사전에서 검색하여 단어와 설명을 출력 (동일 단어 2번 출력 X)
-            string word;
-            vector<string> duplication;
-            
-            while(sstream >> word) {
-                bool duplicated = false;
-                for (auto it_dup = duplication.begin(); it_dup < duplication.end(); it_dup++) {
-                    if (*it_dup == word) {
-                        duplicated = true;
-                        break;
+            stringstream sstream(discriptions[i]);
+            string token;
+            vector<string> duplicatedList;
+            while (sstream >> token) {
+                // 4. 단어의 설명에 등장하는 길이가 3이상인 모든 단어들을 다시 사전에서 검색하여 단어와 설명을 출력 (동일 단어 2번 출력 X)
+                if (token.length() >= 3 && find(duplicatedList.begin(), duplicatedList.end(), token) == duplicatedList.end()) {
+                    for (int j = 0; j < (int)words.size() - 1; j++) {
+                        if (token == words[j]) {
+                            cout << words[j] << ": " << discriptions[j] << endl;
+                            duplicatedList.push_back(words[j]);
+                        }
                     }
                 }
-
-                if (duplicated == true)
-                    continue;
-                
-
             }
-            // break;
+
+            break;  // 찾았으니 반복 종료
         }
     }
-
-   
 
     return 0;
 }
 
 /**
- * @brief 한 라인에서 단어와 설명을 나눠 pair 객체로 반환해주는 함수
- *
- * @param line 대상 라인
- * @param delimiter 구분자
- * @return pair<string, string> first: word, second: discription
+ * @brief 한 라인에서 단어와 설명을 나눠서 vector에 저장하는 함수
+ * 
+ * @param line split할 라인
+ * @param words 단어가 저장될 벡터
+ * @param discriptions 설명이 저장될 벡터
  */
-pair<string, string> split_line(string &line, char delimiter)
-{
+void store_wordsAndDiscriptions(string &line, vector<string> &words, vector<string> &discriptions) {
     stringstream sstream(line);
-    string word, discription;
+    string tmp;
 
-    getline(sstream, word, delimiter);
-    getline(sstream, discription, delimiter);
+    getline(sstream, tmp, '\t');
+    words.push_back(tmp);
 
-    pair<string, string> wordAndDiscription = make_pair(word, discription);
-
-    return wordAndDiscription;
-}
-
-bool isExist(string tmpStr)
-{
-    pair<string, string> p = split_line(tmpStr);
-    return (p.first == str);
+    getline(sstream, tmp, '\t');
+    discriptions.push_back(tmp);
 }
