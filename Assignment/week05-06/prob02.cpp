1b8788246627c4aec8a2bcf6889dbd3a9b0f9c72
SangYeon Yoon <yskite09@gmail.com>
2024-10-07 23:23:54 +0900

    Complete prob02.cpp

--- Assignment/week05-06/prob02.cpp
+++ Assignment/week05-06/prob02.cpp
@@ -29,20 +29,21 @@ void ASC_insert(string item);
 void remove_first();
 void remove_after(Node *prev);
 void remove(int item);
-void DESC_insert(string item);
+Node *DESC_insert(Node *head_ASC);
 
 int main()
 {
     // (1)
     set_linkedList();
-    // print_word(head);
+    print_word(head);
 
     // (2)
     remove(10);
-    // print_word(head);
+    print_word(head);
 
     // (3)
-
+    Node *head_DESC = DESC_insert(head);
+    print_word(head_DESC);
 
     return 0;
 }
@@ -57,7 +58,6 @@ void set_linkedList()
     }
     infile.close();
 }
-
 
 void print_word(Node *head)
 {
@@ -173,14 +173,16 @@ void remove(int item)
     Node *q = nullptr;
     while (p != nullptr)
     {
-        // 
-        if (q == nullptr && p->count <= item) {
+        //
+        if (q == nullptr && p->count <= item)
+        {
             // q가 nullptr - 첫번째 노드가 대상
             remove_first();
             p = head;
             continue;
-        } 
-        else if (p->count <= item) {
+        }
+        else if (p->count <= item)
+        {
             remove_after(q);
             p = q->next;
             continue;
@@ -193,34 +195,115 @@ void remove(int item)
         return;
 }
 
-// 내림차순으로 정렬하며 추가
-void DESC_insert(string item) {
-    Node *p = head;
-    Node *q = nullptr;
+// (2)에서 만든 노드를 떼어서 새로운 정렬된 연결리스트에 삽입하는 방식으로 등장빈도 내림차순으로 정렬하며 추가
+/*
+필요한 것
+빈도수 체크 -> 주소 뽑아오기
+내림차순으로 저장하는 방법
+ */
+Node *DESC_insert(Node *head_ASC)
+{
+    Node *head_DESC = nullptr;
 
-    while (p != nullptr && p->word >= item)
-    {
-        q = p;
-        p = p->next;
-    }
+    while (head_ASC != nullptr)
+    { // 1. 빈도수 체크
+        Node *p_ASC = head_ASC;
+        Node *q_ASC = nullptr;
+        int MAX_count = p_ASC->count;
+        Node *MAX_prePtr = nullptr;
 
-    if (find(item) == nullptr)
-    { // 중복이 없으면
-        if (q == nullptr)
-        { // 첫번째 노드에 들어갈 때
-            add_first(item);
-            head->count = 1;
+        // 같을 때는 어짜피 사전순 대로 옮겨질 것
+        while (p_ASC != nullptr)
+        {
+            if (MAX_count < p_ASC->count)
+            {
+                MAX_count = p_ASC->count;
+                MAX_prePtr = q_ASC; // 나중에 삭제를 위해 전 주소를 받아옴
+            }
+
+            q_ASC = p_ASC;
+            p_ASC = p_ASC->next;
         }
+        
+        // MAX_prePtr는 최고 빈도수를 가지고 있는 노드의 전 주소
+        // 내림차순으로 저장
+        Node *p_DESC = head_DESC;
+        Node *q_DESC = nullptr;
+
+        // MAX_count보다 작아질 때까지 반복
+        while (p_DESC != nullptr && p_DESC->count >= MAX_count)
+        {
+            q_DESC = p_DESC;
+            p_DESC = p_DESC->next;
+        }
+
+        // DESC의 첫 노드에 삽입할 때
+        if (q_DESC == nullptr)
+        {
+            // 삽입 대상이 ASC의 맨 처음 노드일 때
+            if (MAX_prePtr == nullptr)
+            {
+                // DESC에 추가
+                Node *tmp = head_ASC;
+                head_DESC = tmp;
+
+                // ASC에서 삭제
+                head_ASC = head_ASC->next; // 첫번째 노드의 다음 노드를 첫번째 노드로 설정
+                head_DESC->next = nullptr; // = nullptr
+
+                // delete tmp;              // 지우는 게 아니라 DESC로 넘기는 것
+            }
+            // 삽입 대상이 ASC의 그 외의 노드일 때
+            else
+            {
+                // DESC에 추가
+                Node *tmp = MAX_prePtr->next;
+                head_DESC = tmp;
+
+                // ASC에서 삭제
+                MAX_prePtr->next = tmp->next;
+                head_DESC->next = nullptr;
+
+                // delete tmp;      // 지우는 게 아니라 DESC로 넘기는 것
+            }
+        }
+
+        // DESC의 나머지 노드에 삽입할 때
         else
         { // 나머지
-            add_after(q, item);
-            q->next->count = 1;
+
+            // 이상한 경우
+            if (q_DESC == nullptr)
+                return nullptr;
+
+            // 삽입 대상이 ASC의 맨 처음 노드일 때
+            if (MAX_prePtr == nullptr)
+            {
+                // DESC에 추가
+                Node *tmp = head_ASC;
+                q_DESC->next = tmp;
+
+                // ASC에서 삭제
+                head_ASC = head_ASC->next;    // 첫번째 노드의 다음 노드를 첫번째 노드로 설정
+                q_DESC->next->next = nullptr; // = nullptr
+
+                // delete tmp;
+            }
+            // 삽입 대상이 ASC의 그 외의 노드일 때
+            else
+            {
+                // DESC에 추가
+                Node *tmp = MAX_prePtr->next;
+                q_DESC->next = tmp;
+
+                // ASC에서 삭제
+                MAX_prePtr->next = tmp->next;
+                q_DESC->next->next = nullptr;
+
+                // delete tmp;
+            }
         }
     }
-    // else
-    // { // 중복이 있으면
-    //     Node *target = find(item);
-    //     target->count++;
-    // }
-}
 
+    return head_DESC;
+}
