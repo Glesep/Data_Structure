ba85469f3ae51189e193abeacd7017ceedc91e5f
SangYeon Yoon <yskite09@gmail.com>
2024-11-03 21:34:48 +0900

    Save T classArrayStack

--- Lecture/chap_04/template_classArrayStack.cpp
+++ Lecture/chap_04/template_classArrayStack.cpp
@@ -2,53 +2,67 @@
 
 using namespace std;
 
-// const int MAX_CAPACITY = 100;
 template <typename T>
 class ArrayStack
 {
 private:
-    T *stack = new T[4];
+    int stackSize = 4;
+    T *stack = new T[stackSize];
     int top_pos = -1;
 
+
 public:
-
     void ArrayReallocation() {
-        T *tmp = new T[(int)stack.size()*2];
+        T *tmp = new T[stackSize*2];
 
-        for (int i=0; i < stack.size(); i++) {
+        for (int i=0; i < stackSize; i++) {
             tmp[i] = stack[i];
         }
 
-        delete stack;
+        delete[] stack;       // 배열 동적할당 시 [] 추가하는 것 기억하기
         stack = tmp;
+
+        stackSize *= 2;
     }
+
     bool full()
     {
-        return top_pos == MAX_CAPACITY - 1;
+        return top_pos == stackSize - 1;
     }
+
     bool empty()
     {
         return top_pos == -1;
     }
+
     void push(T c)
     {
-        if (full())
-            throw runtime_error("stack_full");
+        if (full()) {
+            ArrayReallocation();
+        }
+            
         stack[++top_pos] = c;
     }
+
     void pop()
     {
         if (empty())
             throw runtime_error("stack_empty");
         top_pos--;
     }
+
     T top()
     {
         if (empty())
             throw runtime_error("stack_empty");
         return stack[top_pos];
     }
+
+    void debug() {
+        cout << stackSize << endl;
+    }
 };
+
 int main()
 {
     ArrayStack<string> s1;
