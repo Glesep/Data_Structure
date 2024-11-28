21f03da0bdd3a7d1d84f4a0c48533a8cc42ae722
SangYeon Yoon <yskite09@gmail.com>
2024-11-28 19:23:55 +0900

    complete prob06_6.cpp

--- Assignment/06/prob06.cpp
+++ Assignment/06/prob06.cpp
@@ -4,63 +4,31 @@
 #include <algorithm>
 using namespace std;
 
-bool IsOnlyOne(string num);
-string numToString(int num);
 string makeOpposite(string num);
-void findTarget(string num, string oppositeNum, string result, int K);
+void findTarget(int currPos, string result, int K);
 
-vector<string> setOfResult;
+// 이진수를 저장하는 전역변수
+string binarySeq, oppositeBinarySeq;
 
 int main()
 {
-    string binarySeq;
     int K;
     cin >> binarySeq;
     cin >> K;
 
-    cout << "h" << endl;
-    findTarget(binarySeq, makeOpposite(binarySeq), "", K);
-    
-
+    oppositeBinarySeq = makeOpposite(binarySeq);
+    findTarget(0, "", K);
     
     
     return 0;
 }
 
-/* 16
-0000
-0001
-0010
-0011
-0100
-0101
-0110
-0111
-1000
-1001
-1010
-1011
-1100
-1101
-1110
-1111
-*/
-/*
-해야 하는 것 
-binarySeq의 길이 N을 찾기 - 벡터로 쪼개기
-*/
-
 /**
- * @brief 전체가 1인가 확인
+ * @brief bitwise not 수행 함수
  * 
- * @param num string 타입의 이진수
- * @return true 1밖에 없음
- * @return false 0이 존재함
+ * @param num 바꾸고 싶은 수
+ * @return string 바꿔진 수
  */
-bool IsOnlyOne(string num) {
-    return num.find_first_not_of("1") == string::npos;
-}
-
 string makeOpposite(string num) {
     string result = "";
     for (int i = 0; i < num.length(); i++) {
@@ -73,28 +41,29 @@ string makeOpposite(string num) {
     return result;
 }
 
-/*
-num과 같은 요소가 없는 변수를 만들기
-K만큼 고르기
-    인덱스를 하나 고르고 다음 인덱스 고르기
-*/
-// 000 11000
-void findTarget(string num, string oppositeNum, string result, int K) {
-    // Base Case
+/**
+ * @brief K개의 비트를 반전시키는 모든 경우를 출력해주는 함수
+ * 
+ * @param startPos 현재 가리키고 있는 인덱스
+ * @param result 결과 값 저장 변수
+ * @param K 남은 비트 반전 횟수
+ */
+void findTarget(int startPos, string result, int K) {
+    // K가 0일 때 - 비트 반전을 다 수행했을 때
     if (K == 0) {
-        // K만큼 바꿨는데 전체를 탐색하지 않았다면
-        if (result.length() != num.length()) {
-            // 부족한 부분은 num의 일부로 채우기
-            result + num.substr(result.length());
-        }
+        // 뒤에 남은 수가 있을 수 있음
+        result += binarySeq.substr(result.length());
         cout << result << endl;
         return;
     }
-    // General Case
-    else {
-        for (int i = 0; i < num.length(); i++) {
-            findTarget(num.remove(i,1), oppositeNum.erase(i,1),
-                            result+oppositeNum[i], K-1);
-        }
+    
+    else if (startPos < binarySeq.length()){
+        // 국소적으로 한 부분만 봤을 때 어떤 케이스가 있는지를 생각 후 전체적으로 넓게 보기
+
+        // 자기 위치에서 바꾸거나
+        findTarget(startPos+1, result+oppositeBinarySeq[startPos], K-1);
+        // 자기 위치에서는 안바꾸거나 (마지막 비트에서 접근하면 startPos가 oppositeBinarySeq의 인덱스를 넘어가면서 오류)
+        findTarget(startPos+1, result+binarySeq[startPos], K);
     }
+    
 }
