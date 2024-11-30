a3e8700aa7d342690f3a5b43efd0fdbb76b76bda
Glesep <yskite09@gmail.com>
2024-11-30 17:23:41 +0900

    feat: add func canCannonGo

--- Assignment/06/prob08.cpp
+++ Assignment/06/prob08.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void pullOneCase(ifstream &infile);
 void printGround();
 pair<int, int> pullPos(ifstream &infile);
+bool moveable(pair<int, int> currPos, pair<int, int> targetPos, int dir, int cntObject=0);
 
 int groundSize = 0;
 vector<vector<int>> ground;
@@ -20,9 +21,6 @@ int offset[4][2] = {{-1, 0},  // 북
 포는 무조건 장애물을 하나만 넘어야 이동 가능
  */
 
-
-
-
 int main()
 {
     ifstream infile("input8.txt");
@@ -32,8 +30,13 @@ int main()
     pair<int, int> targetPos = pullPos(infile);
 
     infile.close();
-    
-    
+    if (moveable(pair<int, int>(1, 0), pair<int, int>(1, 7), 1, 0))
+    {
+        cout << "yes" << endl;
+    }
+    else
+        cout << "no" << endl;
+
     return 0;
 }
 
@@ -41,29 +44,85 @@ int main()
 한놈 넘긴 표시를 해야됨
 */
 
-bool moveable(pair<int, int> currPos, pair<int, int> targetPos, int dir, bool isOnlyOneObject) {
-    // isOnlyOneObject: 경로에 하나의 장애물만 있는지 판단하는 flag
+/**
+ * @brief currPos에서 targetPos까지 갈 수 있는지 확인(일직선상에서만 가능)
+ *
+ * @param currPos 시작 위치
+ * @param targetPos 도착 위치
+ * @param dir 방향(이 방향으로만 감)
+ * @param cntObject 경로에 하나의 장애물만 있는지 판단을 위한 정수형 변수, 초깃값 0
+ * @return true
+ * @return false
+ */
+bool moveable(pair<int, int> currPos, pair<int, int> targetPos, int dir, int cntObject)
+{
+    // cntObject: 경로에 하나의 장애물만 있는지 판단을 위한 정수형 변수
 
-    // 목적지 자체가 장애물이라면
-    if (ground[targetPos.first][targetPos.second] == 1)
+    // 목적지 자체가 갈 수 없는 곳이라면
+    if (ground[targetPos.first][targetPos.second] != 0)
         return false;
 
-    // 현재 위치가 목적지라면 flag 출력
-    if (currPos == targetPos) {
-        return isOnlyOneObject;
-    }
-    else {
-        // 현재 위치에 장애물이 존재한다면
-        if (ground[currPos.first][currPos.second] == 1) {
-            isOnlyOneObject = !isOnlyOneObject;
+    // 현재 위치가 목적지라면
+    if (currPos == targetPos)
+    {
+        // 장애물을 한번 넘었을 때
+        if (cntObject == 1)
+        {
+            return true;
         }
-
-        return moveable(pair<int, int> (currPos.first+offset[dir][0],
-                                        currPos.first+offset[dir][1]),
-                        targetPos, dir, isOnlyOneObject);
+        else
+            return false;
     }
+    // 갈 수 있는 곳에 접근했을 때
+    else if (currPos.first >= 0 &&
+             currPos.first < groundSize &&
+             currPos.second >= 0 &&
+             currPos.second < groundSize)
+    {
+        // 현재 위치에 장애물이 존재한다면
+        if (ground[currPos.first][currPos.second] == 1)
+        {
+            return moveable(pair<int, int>(currPos.first + offset[dir][0],
+                                           currPos.second + offset[dir][1]),
+                            targetPos, dir, cntObject + 1);
+        }
+        else
+            return moveable(pair<int, int>(currPos.first + offset[dir][0],
+                                           currPos.second + offset[dir][1]),
+                            targetPos, dir, cntObject);
+    }
+
+    // 그 이외에는 못 감
+    return false;
 }
 
+// 갈 수 있으면 움직이고, 못가면 다음 칸 보기
+void canCannonGo(pair<int, int> currPos, pair<int, int> targetPos, bool &isFound)
+{
+    // 길을 못찾았을 때만 수행
+    if (!isFound)
+    {
+        if (currPos == targetPos)
+        {
+            isFound = true;
+            cout << "yes" << endl;
+            return;
+        }
+
+        for (int dir = 0; dir < 4; dir++) {
+            pair<int, int> tmpPos(currPos.first + offset[dir][0],
+                                                currPos.second + offset[dir][1]);
+
+            if (moveable(currPos, tmpPos, dir)) {
+                ground[currPos.first][currPos.second] = -1;
+                canCannonGo(tmpPos, targetPos, isFound);
+            }
+            
+        }
+        ground[currPos.first][currPos.second] = 0;
+
+    }
+}
 /**
  * @brief 하나의 테스트 케이스를 불러오는 함수
  *
@@ -86,11 +145,18 @@ void pullOneCase(ifstream &infile)
     }
 }
 
-pair<int, int> pullPos(ifstream &infile) {
+/**
+ * @brief 파일에서 위치 정보 추출하는 함수
+ *
+ * @param infile ifstream
+ * @return pair<int, int> 위치 정보
+ */
+pair<int, int> pullPos(ifstream &infile)
+{
     int x, y;
     infile >> x >> y;
 
-    return pair<int,int> (x, y);
+    return pair<int, int>(x, y);
 }
 
 /**
